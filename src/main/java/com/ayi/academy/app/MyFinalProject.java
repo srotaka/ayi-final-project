2dbc202eba0c5e87230a1b4f0038eceb781c07bb
Silvia R. Otaka <srotaka@gmail.com>
2022-09-28 20:44:17 -0300

    Initial commit

---
+++ src/main/java/com/ayi/academy/app/MyFinalProject.java
@@ -0,0 +1,14 @@
+package com.ayi.academy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinalProject {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyFinalProject.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayi/academy/app/MyFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayi.academy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
