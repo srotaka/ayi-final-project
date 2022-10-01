5a3a9d6ce4f7e84c3941cee33f9d48631b55b7b0
Silvia R. Otaka <srotaka@gmail.com>
2022-10-01 13:57:34 -0300

    Repositories created

---
+++ src/main/java/com/ayi/academy/app/repositories/IBillRepository.java
@@ -0,0 +1,8 @@
+package com.ayi.academy.app.repositories;
+import com.ayi.academy.app.entities.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBillRepository extends JpaRepository<Bill, Integer> {
+}
